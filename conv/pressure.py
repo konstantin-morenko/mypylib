7f0caf6266cc0e6e3aca2bac1f7ce8ba7fe7cbca
Konstantin Morenko <me@konstantin-morenko.ru>
2020-03-04 22:05:35 +0300

    Temp conv

---
+++ conv/pressure.py
@@ -0,0 +1,10 @@
+"""Pressure converting"""
+
+def mmhg2pa(mmhg):
+    pass
+
+def bar2pa(bar):
+    pass
+
+def mbar2pa(mbar):
+    pass
---
+++ conv/temp.py
@@ -0,0 +1,9 @@
+"""Temperature converting."""
+
+def C2K(c):
+    """Celsius to Kelvins."""
+    return c + 273.15
+
+def K2C(k):
+    """Kelvins to Celsius."""
+    return k - 273.15
