1f9ed7d4300950716cbd523a44ff54504ba711eb
Konstantin Morenko <me@konstantin-morenko.ru>
2020-04-07 16:45:10 +0300

    Continuous list of wind speeds

--- renew/wind/rayleigh.py
+++ renew/wind/rayleigh.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 class Rayleigh:
     def __init__(self, name, avr):
@@ -23,3 +24,23 @@ class Rayleigh:
             if prob < 1 - math.exp(-(speed**2)/(2*self.sigma**2)):
                 return speed - 1
         return 20
+
+    def nxt(self, speed):
+        spd_lst = []
+        for i in [x/10 for x in range(-10, 10)]:
+            spd_nxt = speed + i
+            if spd_nxt <= 0:
+                spd_nxt = 0.1
+            spd_lst.append({'spd': spd_nxt, 'prob': self.prob(spd_nxt) / 10})
+        # Normalizing
+        summ = sum([x['prob'] for x in spd_lst])
+        for spd in spd_lst:
+            spd['prob'] /= summ
+        # Random number
+        rnd = random.random()
+        summ = 0
+        i = 0
+        while summ < rnd:
+            summ += spd_lst[i]['prob']
+            i += 1
+        return spd_lst[i-1]['spd']
