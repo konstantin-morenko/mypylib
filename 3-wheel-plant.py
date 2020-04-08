2d59c362c3df9348c567c2d12b32d606d7729ba2
Konstantin Morenko <me@konstantin-morenko.ru>
2020-04-08 16:58:42 +0300

    Output to dat and gnuplot

--- 3-wheel-plant.py
+++ 3-wheel-plant.py
@@ -28,11 +28,28 @@ wind_lst = [0.1]
 for i in range(0, args.length):
     wind_lst.append(rl.nxt(wind_lst[-1]))
 
-for wind in wind_lst:
-    pwr = 0
-    for wheel in wheel_list:
-        wind_pwr = wheel.power(wind) * size
-        pwr += acc.flow(wind_pwr)
-    print("{:.1f} {:.0f} {:.0f} {:.0f}".format(wind, wind_pwr, pwr, acc.pwr))
+with open('pwr.dat', 'w') as f:
+    for wind in wind_lst:
+        pwr = 0
+        for wheel in wheel_list:
+            wind_pwr = wheel.power(wind) * size
+            pwr += acc.flow(wind_pwr)
+        f.write("{:.1f} {:.0f} {:.0f} {:.0f}\n".format(wind, wind_pwr, pwr, acc.pwr))
+
+with open('plot.gnuplot', 'w') as f:
+    f.write("""set term png
+    set output 'pwr.png'
+    set title 'Мощность ветра, мощность на выходе и запас энергии в аккумуляторе (зерно={seed}, длина={length})'
+    set xlabel 'Точки во времени'
+    set ylabel 'Мощность ветра и на выходе'
+    set y2tics 0, 1000
+    set y2label 'Энергия в аккумуляторе'
+    set terminal png size 2000, 1000
+    set ytics nomirror
+    plot 'pwr.dat' using 0:2 with line title 'Мощность ветра' axis x1y1, \
+    'pwr.dat' using 0:3 with line title 'Мощность на выходе ({out_pwr})' axis x1y1, \
+    'pwr.dat' using 0:4 with line title 'Энергия в аккумуляторе (емкость={acc_cap})' axis x1y2
+    set output""".format(seed=args.seed, length=args.length, out_pwr=args.output, acc_cap=args.capacity))
+
 
 
