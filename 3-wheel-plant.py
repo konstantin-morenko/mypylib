011d1916c7d24da8c5c2ad1cc2e7ac005f6b0ff2
Konstantin Morenko <me@konstantin-morenko.ru>
2020-04-08 16:58:30 +0300

    Command-line arguments

--- 3-wheel-plant.py
+++ 3-wheel-plant.py
@@ -1,8 +1,21 @@
 import renew.wind.wheel as Wheel
 import renew.acc.acc as Acc
 import renew.wind.rayleigh as Ray
+import random
 
-acc = Acc.Valve(15000, 4000, 700)
+import argparse
+parser = argparse.ArgumentParser(description='Plot power graph')
+parser.add_argument('--length', type=int, help='length of calcs', default=200)
+parser.add_argument('--capacity', type=int, help='accumulator capacity', default=15000)
+parser.add_argument('--output', type=int, help='output energy', default=700)
+parser.add_argument('seed', type=int, help='random seed')
+
+args = parser.parse_args()
+
+random.seed(args.seed)
+
+
+acc = Acc.Valve(args.capacity, args.capacity / 2, args.output)
 
 wheel_list = [Wheel.Wheel("2-blade", 8, 0.47)]
 
@@ -12,7 +25,7 @@ avr = 0
 rl = Ray.Rayleigh('8 average', 8.0)
 wind_lst = [0.1]
 
-for i in range(0, 2000):
+for i in range(0, args.length):
     wind_lst.append(rl.nxt(wind_lst[-1]))
 
 for wind in wind_lst:
